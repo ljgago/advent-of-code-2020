dc03b363911424858d1b5584eab65930227413f2
Leonardo Javier Gago <ljgago@gmail.com>
2020-12-06 02:47:27 -0300

    REFACTOR: Changes the base input reader

--- src/input.rs
+++ src/input.rs
@@ -1,25 +1,14 @@
 // input.rs
-// input.rs
 
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::str::FromStr;
 
-pub fn read<T>(path: &str) -> std::io::Result<impl Iterator<Item = T>>
+pub fn read_by_line<T>(filename: &str) -> Vec<T>
 where
     T: FromStr,
 {
-    let file = File::open(path)?;
-    let mut reader = BufReader::new(file);
-    let mut buf = String::new();
-    Ok(std::iter::from_fn(move || {
-        buf.clear();
-        reader
-            .read_line(&mut buf)
-            .map_err(|_| ())
-            .and_then(|_| T::from_str(buf.trim()).map_err(|_| ()))
-            .ok()
-    })
-    )
+    std::fs::read_to_string(filename)
+        .expect("file not found!")
+        .lines()
+        .flat_map(|x| x.parse())
+        .collect()
 }
